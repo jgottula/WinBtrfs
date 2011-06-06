3a334c15d19886fdd40f0e20a3b3c219eb8f122c
Justin Gottula <justin@jgottula.com>
2011-06-06 15:09:54 -0700

    Added new constants for st_mode stat value.

--- WinBtrfsCLI/constants.h
+++ WinBtrfsCLI/constants.h
@@ -1,3 +1,4 @@
+/* Btrfs objectIDs */
 #define OBJID_ROOT_TREE			0x01
 #define OBJID_EXTENT_TREE		0x02
 #define OBJID_CHUNK_TREE		0x03
@@ -14,6 +15,7 @@
 #define OBJID_EXTENT_CSUM		-0x0a
 #define OBJID_MULTIPLE			-0x100
 
+/* Btrfs item types */
 #define TYPE_INODE_ITEM			0x01
 #define TYPE_INODE_REF			0x0c
 #define TYPE_XATTR_ITEM			0x18
@@ -39,13 +41,41 @@
 #define TYPE_CHUNK_ITEM			0xe4
 #define TYPE_STRING_ITEM		0xfd
 
+/* Btrfs superblock physical addresses */
 #define SUPERBLOCK_1_PADDR		0x10000
 #define SUPERBLOCK_2_PADDR		0x4000000
 #define SUPERBLOCK_3_PADDR		0x4000000000
 #define SUPERBLOCK_4_PADDR		0x4000000000000
 
+/* address types */
 #define ADDR_PHYSICAL			0x00
 #define ADDR_LOGICAL			0x01
 
+/* FS tree parser operations */
 #define FSOP_NAME_TO_ID			0x00
 #define FSOP_ID_TO_INODE		0x01
+
+/* stat mode constants */
+#define S_IFMT					0170000	// bitmask for file type fields
+#define S_IFSOCK				0140000	// socket
+#define S_IFLNK					0120000	// symbolic link
+#define S_IFREG					0100000	// regular file
+#define S_IFBLK					0060000	// block device
+#define S_IFDIR					0040000	// directory
+#define S_IFCHR					0020000	// character device
+#define S_IFIFO					0010000	// FIFO
+#define S_ISUID					0004000	// set UID bit
+#define S_ISGID					0002000	// set GID bit
+#define S_ISVTX					0001000	// sticky bit
+#define S_IRWXU					00700	// mask for owner's permissions
+#define S_IRUSR					00400	// owner has read permission
+#define S_IWUSR					00200	// owner has write permission
+#define S_IXUSR					00100	// owner has execute permission
+#define S_IRWXG					00070	// mask for group's permissions
+#define S_IRGRP					00040	// group has read permission
+#define S_IWGRP					00020	// group has write permission
+#define S_IXGRP					00010	// group has execute permission
+#define S_IRWXO					00007	// mask for others' permissions
+#define S_IROTH					00004	// others have read permission
+#define S_IWOTH					00002	// others have write permission
+#define S_IXOTH					00001	// others have execute permission
