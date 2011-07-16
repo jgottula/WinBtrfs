e72b880f7f1b0cc59a64e5cd16040f58ab8e723c
Justin Gottula <justin@jgottula.com>
2011-07-15 18:18:54 -0700

    Fixed WinBtrfsLib build error by adding a DLLMain function.

---
+++ WinBtrfsLib/lib.cpp
@@ -0,0 +1,22 @@
+/* WinBtrfsLib/lib.cpp
+ * ???
+ *
+ * WinBtrfs
+ *
+ * Copyright (c) 2011 Justin Gottula
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ */
+
+#include <cstdio>
+#include <Windows.h>
+
+BOOL WINAPI DLLMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	printf("WinBtrfsLib is a stub!\n");
+	
+	return TRUE;
+}
