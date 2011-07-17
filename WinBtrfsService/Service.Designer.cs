92c2dee6d40c926d042429a33df8bf1af1728e91
Justin Gottula <justin@jgottula.com>
2011-07-16 19:58:58 -0700

    Started working on WinBtrfsService.

--- WinBtrfsService/Service.Designer.cs
+++ WinBtrfsService/Service.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinBtrfsService
 {
-    partial class Service1
+    partial class Service
     {
         /// <summary> 
         /// Required designer variable.
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.ServiceName = "Service1";
+            // 
+            // Service
+            // 
+            this.ServiceName = "WinBtrfsService";
+
         }
 
         #endregion
--- WinBtrfsService/Service.cs
+++ WinBtrfsService/Service.cs
@@ -1,27 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
 using System.ServiceProcess;
-using System.Text;
 
 namespace WinBtrfsService
 {
-    public partial class Service1 : ServiceBase
+    public partial class Service : ServiceBase
     {
-        public Service1()
+        public Service()
         {
             InitializeComponent();
         }
 
         protected override void OnStart(string[] args)
         {
+
         }
 
         protected override void OnStop()
         {
+
         }
     }
 }
