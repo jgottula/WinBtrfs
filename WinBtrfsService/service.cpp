9541f0b95bab4762a546a124bf46d521490dab86
Justin Gottula <justin@jgottula.com>
2011-07-16 21:03:32 -0700

    Added a ServiceMain callback to WinBrtfsService.

--- WinBtrfsService/service.cpp
+++ WinBtrfsService/service.cpp
@@ -12,9 +12,30 @@
 
 #include <Windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+namespace WinBtrfsService
 {
+	typedef VOID (WINAPI *LPSERVICE_MAIN_FUNCTIONW)(
+	DWORD   dwNumServicesArgs,
+	LPWSTR  *lpServiceArgVectors
+	);
 
+	void WINAPI serviceMain(DWORD dwNumServicesArgs, LPWSTR *lpServiceArgVectors)
+	{
+
+	}
+}
+
+int main(int argc, char **argv)
+{
+	SERVICE_TABLE_ENTRY serviceTable[2];
+
+	serviceTable[0].lpServiceName = L"WinBtrfsService";
+	serviceTable[0].lpServiceProc = &WinBtrfsService::serviceMain;
+
+	serviceTable[1].lpServiceName = NULL;
+	serviceTable[1].lpServiceProc = NULL;
+
+	StartServiceCtrlDispatcher(serviceTable);
 
 	return 0;
 }
