0eb9b79681984f20d81782ff8c6cd14da8d4a951
Justin Gottula <justin@jgottula.com>
2011-07-16 20:49:53 -0700

    Re-added WinBtrfsService as a C++ project.

---
+++ WinBtrfsService/service.cpp
@@ -0,0 +1,20 @@
+/* WinBtrfsService/service.cpp
+ * Windows service code
+ *
+ * WinBtrfs
+ * Copyright (c) 2011 Justin Gottula
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ */
+
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+
+	return 0;
+}
