e5b8a1fa2ee111aab55234eb5f90ac3e73e681ee
Justin Gottula <justin@jgottula.com>
2011-07-16 21:19:26 -0700

    Added a service control handler for WinBtrfsService.

--- WinBtrfsService/service.cpp
+++ WinBtrfsService/service.cpp
@@ -10,32 +10,47 @@
  * any later version.
  */
 
+#include <cassert>
 #include <Windows.h>
 
 namespace WinBtrfsService
 {
-	typedef VOID (WINAPI *LPSERVICE_MAIN_FUNCTIONW)(
-	DWORD   dwNumServicesArgs,
-	LPWSTR  *lpServiceArgVectors
-	);
-
+	DWORD WINAPI serviceCtrlHandler(DWORD dwControl, DWORD dwEventType,
+		LPVOID lpEventData, LPVOID lpContext)
+	{
+		switch (dwControl)
+		{
+		default:
+			return ERROR_CALL_NOT_IMPLEMENTED;
+		}
+	}
+	
 	void WINAPI serviceMain(DWORD dwNumServicesArgs, LPWSTR *lpServiceArgVectors)
 	{
+		SERVICE_STATUS status;
 
+		status.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+		status.dwCurrentState = SERVICE_STOPPED;
+		status.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+		status.dwWin32ExitCode = NO_ERROR;
+		status.dwServiceSpecificExitCode = NO_ERROR;
+		status.dwCheckPoint = 0;
+		status.dwWaitHint = 0;
+
+		SERVICE_STATUS_HANDLE hStatus = RegisterServiceCtrlHandlerEx(L"WinBtrfsService",
+			&serviceCtrlHandler, NULL);
+		assert(hStatus != 0);
 	}
 }
 
 int main(int argc, char **argv)
 {
-	SERVICE_TABLE_ENTRY serviceTable[2];
+	SERVICE_TABLE_ENTRY serviceTable[] =
+	{
+		{ L"WinBtrfsService", &WinBtrfsService::serviceMain },
+		{ NULL, NULL }
+	};
 
-	serviceTable[0].lpServiceName = L"WinBtrfsService";
-	serviceTable[0].lpServiceProc = &WinBtrfsService::serviceMain;
-
-	serviceTable[1].lpServiceName = NULL;
-	serviceTable[1].lpServiceProc = NULL;
-
-	StartServiceCtrlDispatcher(serviceTable);
-
-	return 0;
+	/* this function will not return until all of this process's services have stopped */
+	return StartServiceCtrlDispatcher(serviceTable);
 }
