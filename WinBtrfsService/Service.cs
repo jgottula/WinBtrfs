030eac27e58daa52992ea950fda8b4e535890972
Justin Gottula <justin@jgottula.com>
2011-07-25 15:23:31 -0700

    Added more robust exception handling code on the server side.

--- WinBtrfsService/Service.cs
+++ WinBtrfsService/Service.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.IO.Pipes;
 using System.ServiceProcess;
 using System.Threading;
@@ -56,6 +57,7 @@ namespace WinBtrfsService
 					{
 						pipeServer.EndWaitForConnection(result);
 						GotConnection();
+						pipeServer.Disconnect();
 						break;
 					}
 					else
@@ -67,23 +69,68 @@ namespace WinBtrfsService
 		private void GotConnection()
 		{
 			byte[] buffer = new byte[102400];
-			int bufferLen = pipeServer.Read(buffer, 0, buffer.Length);
+			int bufferLen = 0;
+
+			try
+			{
+				bufferLen = pipeServer.Read(buffer, 0, buffer.Length);
+			}
+			catch (ObjectDisposedException)
+			{
+				Program.eventLog.WriteEntry("The pipe closed unexpectedly when reading.",
+					EventLogEntryType.Warning);
+				return;
+			}
+			catch (InvalidOperationException)
+			{
+				Program.eventLog.WriteEntry("The pipe was in an unexpected state at read time.",
+					EventLogEntryType.Warning);
+				return;
+			}
+			catch (IOException e)
+			{
+				Program.eventLog.WriteEntry("Caught an IOException at read time: " + e.Message,
+					EventLogEntryType.Warning);
+				return;
+			}
 
 			if (pipeServer.IsMessageComplete)
 			{
 				String str = System.Text.Encoding.Unicode.GetString(buffer, 0, bufferLen);
 				Program.eventLog.WriteEntry("Got a pipe connection. Message: " + str, EventLogEntryType.Information);
 
+				/* take me out! */
+				Thread.Sleep(9000);
+
 				byte[] response = System.Text.Encoding.Unicode.GetBytes("OK");
-				pipeServer.Write(response, 0, response.Length);
+				try
+				{
+					pipeServer.Write(response, 0, response.Length);
+				}
+				catch (ObjectDisposedException)
+				{
+					Program.eventLog.WriteEntry("The pipe closed unexpectedly when writing.",
+						EventLogEntryType.Warning);
+					return;
+				}
+				catch (InvalidOperationException)
+				{
+					Program.eventLog.WriteEntry("The pipe was in an unexpected state at write time.",
+						EventLogEntryType.Warning);
+					return;
+				}
+				catch (IOException e)
+				{
+					Program.eventLog.WriteEntry("Caught an IOException at write time: " + e.Message,
+						EventLogEntryType.Warning);
+					return;
+				}
 			}
 			else
 			{
 				Program.eventLog.WriteEntry("A message larger than 100K arrived; discarding.",
 					EventLogEntryType.Warning);
 			}
-
-			pipeServer.Disconnect();
 		}
 	}
 }
