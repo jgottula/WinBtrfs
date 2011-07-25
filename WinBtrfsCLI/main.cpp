ac02f05f4b74c5bf1a773ee35ea755f565393843
Justin Gottula <justin@jgottula.com>
2011-07-24 22:56:28 -0700

    Moved error messages to stderr.
    Added a function for pipe error messages.
    Started moving PipeClient code over.

--- WinBtrfsCLI/main.cpp
+++ WinBtrfsCLI/main.cpp
@@ -24,7 +24,7 @@ namespace WinBtrfsCLI
 {
 	void usage()
 	{
-		printf("\n\nUsage: WinBtrfsCLI.exe <command> [parameters]\n\n"
+		puts("\n\nUsage: WinBtrfsCLI.exe <command> [parameters]\n\n"
 			"Commands: mount, [more to be added]\n\n"
 			"WinBtrfsCLI.exe mount [options] <mount point> <device> [<device> ...]\n"
 			"Options:\n"
@@ -41,15 +41,90 @@ namespace WinBtrfsCLI
 		va_list args;
 
 		va_start(args, format);
-		vprintf(format, args);
+		vfprintf(stderr, format, args);
 		va_end(args);
 
 		usage();
 	}
 
+	void pipeError(int rtn, const char *format, ...)
+	{
+		va_list args;
+
+		fputs("Failed to communicate with WinBtrfsService!\n", stderr);
+
+		va_start(args, format);
+		vfprintf(stderr, format, args);
+		va_end(args);
+
+		exit(rtn);
+	}
+
 	void sendMountMsg(MountData *mountData)
 	{
-		printf("sendMountMsg: unimplemented!\n");
+		HANDLE hPipe = INVALID_HANDLE_VALUE;
+
+		if (WaitNamedPipe(L"\\\\.\\pipe\\WinBtrfsService", 1000) == 0)
+			pipeError(GetLastError(), "WaitNamedPipe failed: %u\n", GetLastError());
+
+		if ((hPipe = CreateFile(L"\\\\.\\pipe\\WinBtrfsService", GENERIC_READ | GENERIC_WRITE,
+			0, NULL, OPEN_EXISTING, 0, NULL)) == INVALID_HANDLE_VALUE)
+			pipeError(GetLastError(), "CreateFile failed: %u\n", GetLastError());
+		
+#if 0
+		const size_t DATA_LEN = sizeof(MountData) + (2 * MAX_PATH * sizeof(wchar_t));
+		ServiceMsg *msg = (ServiceMsg *)malloc(sizeof(ServiceMsg) + DATA_LEN);
+		MountData *mountData = (MountData *)msg->data;
+		DWORD bytesWritten = 0;
+
+		mountData->dumpOnly = false;
+		mountData->noDump = false;
+		mountData->useSubvolID = true;
+		mountData->useSubvolName = false;
+		mountData->subvolID = (BtrfsObjID)256;
+		mountData->subvolName[0] = 0;
+		wcscpy(mountData->mountPoint, L"X:");
+		mountData->numDevices = 2;
+		wcscpy(mountData->devicePaths[0], L"..\\..\\test_images\\btrfs_multiA.img");
+		wcscpy(mountData->devicePaths[1], L"..\\..\\test_images\\btrfs_multiB.img");
+
+		msg->type = MSG_QUERY_MOUNT;
+		msg->dataLen = DATA_LEN;
+
+		if (WriteFile(hPipe, msg, sizeof(ServiceMsg) + DATA_LEN, &bytesWritten, NULL) == 0)
+		{
+			fprintf(stderr, "WriteFile failed: %u\n", GetLastError());
+			CloseHandle(hPipe);
+			return 3;
+		}
+
+		free(msg);
+
+		printf("Sent REQ_MOUNT.\n", msg);
+
+		msg = (ServiceMsg *)malloc(sizeof(ServiceMsg) + sizeof(int));
+		DWORD bytesRead = 0;
+
+		if (ReadFile(hPipe, msg, sizeof(ServiceMsg) + sizeof(int), &bytesRead, NULL) == 0)
+		{
+			fprintf(stderr, "ReadFile failed: %u\n", GetLastError());
+			CloseHandle(hPipe);
+			return 4;
+		}
+
+		if (msg->type == MSG_REPLY_OK)
+			printf("Received MSG_REPLY_OK.\n");
+		else if (msg->type == MSG_REPLY_ERROR)
+			printf("Received MSG_REPLY_ERROR (%d).\n", *((int *)msg->data));
+
+		free(msg);
+
+		CloseHandle(hPipe);
+
+		printf("Closed the connection.\n");
+
+		return 0;
+#endif
 	}
 	
 	void handleArgs(int argc, char **argv)
