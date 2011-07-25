4b5e6d3b186cd4e2607a6dd65d89a342fbed687b
Justin Gottula <justin@jgottula.com>
2011-07-24 23:49:23 -0700

    Rearranged MsgType values and set the enum size so that the GUI side doesn't have to deal with variances.

--- WinBtrfsService/ipc.h
+++ WinBtrfsService/ipc.h
@@ -17,22 +17,23 @@
 
 namespace WinBtrfsService
 {
-	enum MsgType
+	/* these values MUST NOT change! */
+	enum MsgType : int
 	{
-		MSG_QUERY_MOUNT,		/* from frontend: requests that a volume be mounted
-									data should be a MountData struct (variably sized) */
-		MSG_QUERY_DRV_START,	/* from backend: requests information about the volume to mount
-									no data */
-		MSG_QUERY_FS_UUID,		/* from backend: ensures that no volumes are mounted in multiple locations
-									data should be a UUID (0x10 bytes) */
-		MSG_QUERY_DRV_FAILURE,	/* from backend: indicates a fatal problem; the volume entry should be removed
-									no data */
-		MSG_REPLY_OK,			/* indicates success
-									no data */
-		MSG_REPLY_OK_DATA,		/* indicates success; also carries data
-									data depends on the query */
-		MSG_REPLY_ERROR			/* indicates an error
-									data is an int representing an error code (may be MountError or other enum) */
+		MSG_REPLY_OK = 0,			/* indicates success
+										no data */
+		MSG_REPLY_OK_DATA = 1,		/* indicates success; also carries data
+										data depends on the query */
+		MSG_REPLY_ERROR = 2,		/* indicates an error
+										data is an int representing an error code (may be MountError or other enum) */
+		MSG_QUERY_MOUNT = 3,		/* from frontend: requests that a volume be mounted
+										data should be a MountData struct (variably sized) */
+		MSG_QUERY_DRV_START = 4,	/* from backend: requests information about the volume to mount
+										no data */
+		MSG_QUERY_FS_UUID = 5,		/* from backend: ensures that no volumes are mounted in multiple locations
+										data should be a UUID (0x10 bytes) */
+		MSG_QUERY_DRV_FAILURE = 6,	/* from backend: indicates a fatal problem; the volume entry should be removed
+										no data */
 	};
 
 	enum MountError : int
