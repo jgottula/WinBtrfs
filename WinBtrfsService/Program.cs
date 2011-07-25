854a38b5ba39bbee36106591e133366297da25b7
Justin Gottula <justin@jgottula.com>
2011-07-24 23:55:20 -0700

    Removed WinBtrfsService from the project and moved the folder contents to WinBtrfsService-deprecated.
    Added WinBtrfsService, a .NET 4.0 service.

---
+++ WinBtrfsService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace WinBtrfsService
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			ServiceBase[] ServicesToRun;
+			ServicesToRun = new ServiceBase[] 
+			{ 
+				new Service() 
+			};
+			ServiceBase.Run(ServicesToRun);
+		}
+	}
+}
---
+++ WinBtrfsService/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace WinBtrfsService
+{
+	public partial class Service : ServiceBase
+	{
+		public Service()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+		}
+
+		protected override void OnStop()
+		{
+		}
+	}
+}
