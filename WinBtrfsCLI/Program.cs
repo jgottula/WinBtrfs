6b8aeccde5f2c6db60df9f73ca766c5ead7739a1
Justin Gottula <justin@jgottula.com>
2011-07-25 14:09:24 -0700

    Added a usage statement to WinBtrfsCLI.

--- WinBtrfsCLI/Program.cs
+++ WinBtrfsCLI/Program.cs
@@ -7,6 +7,29 @@ namespace WinBtrfsCLI
 		static void Main(string[] args)
 		{
 			Console.WriteLine("WinBtrfsCLI: .NET Stub");
+
+			Usage();
+		}
+
+		static void Usage()
+		{
+			Console.Write("\n\nUsage: WinBtrfsCLI.exe <command> [parameters]\n\n" +
+				"Commands: mount, [more to be added]\n\n" +
+				"WinBtrfsCLI.exe mount [options] <mount point> <device> [<device> ...]\n" +
+				"Options:\n" +
+				"--subvol=<name>  mount the subvolume with the given name (case-sensitive)\n" +
+				"--subvol-id=<id> mount the subvolume with the given ID\n" +
+				"--dump=<file>    dump filesystem trees to the given file\n" +
+				"--test-run       stop just short of actually mounting the volume\n");
+			
+			Environment.Exit(1);
+		}
+
+		static void UsageError(string msg)
+		{
+			Console.WriteLine(msg);
+
+			Usage();
 		}
 	}
 }
