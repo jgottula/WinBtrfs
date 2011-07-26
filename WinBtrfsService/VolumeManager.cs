102153f2833d99ba87e064da088fede880fd3a17
Justin Gottula <justin@jgottula.com>
2011-07-25 17:13:36 -0700

    Added initial code for VolumeManager's Mount method.

--- WinBtrfsService/VolumeManager.cs
+++ WinBtrfsService/VolumeManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace WinBtrfsService
 {
@@ -7,6 +9,7 @@ namespace WinBtrfsService
 	{
 		public MountData mountData = new MountData();
 		public Guid fsUUID = new Guid();
+		public Process drvProc = null;
 	}
 
 	class MountData
@@ -20,5 +23,26 @@ namespace WinBtrfsService
 	static class VolumeManager
 	{
 		public static List<VolumeEntry> volumeTable = new List<VolumeEntry>();
+
+		public static string Mount(string[] lines)
+		{
+			var entry = new VolumeEntry();
+			/* TODO: populate the mountData fields from information in lines */
+
+			try
+			{
+				entry.drvProc = Process.Start("WinBtrfsDrv.exe",
+					"--pipe-name=WinBtrfsService --parent-pid=" + Process.GetCurrentProcess().Id.ToString());
+			}
+			catch (FileNotFoundException)
+			{
+				Program.eventLog.WriteEntry("Could not find WinBtrfsDrv.exe!", EventLogEntryType.Error);
+				return "Error\nCould not find WinBtrfsDrv.exe!";
+			}
+
+			volumeTable.Add(entry);
+
+			return "OK";
+		}
 	}
 }
